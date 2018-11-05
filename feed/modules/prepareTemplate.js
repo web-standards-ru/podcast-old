9c0157c0a9db4ccf33fa1c4e24c2f47e39f21225
Mikhail Baranov <mikhail.baranov@gmail.com>
2018-11-05 16:24:32 +0300

    Переводит время выхода эпизода на 9 утра по Москве (#212)

--- feed/modules/prepareTemplate.js
+++ feed/modules/prepareTemplate.js
@@ -36,7 +36,16 @@ module.exports = (file) => {
                 paragraphs[1].match(/.+:/)[0]
                     .replace(':', '') :
                 '';
-            const date = moment(dateRaw, DATE_PARSE_FORMAT).tz('Europe/Moscow').locale('en').format(DATE_SHOW_FORMAT);
+            const date = moment(dateRaw, DATE_PARSE_FORMAT)
+                .set({
+                    hours: 9,
+                    minutes: 0,
+                    seconds: 0,
+                })
+                .tz('Europe/Moscow')
+                .locale('en')
+                .format(DATE_SHOW_FORMAT);
+
             const html = converter
                 .makeHtml(content.replace(/^#.*\n\n/, '# '))
                 // Replace first available header with <p></p>
